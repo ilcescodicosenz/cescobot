84dc9e55ec5ace0c6793c3492603beaff63959d0
ilcescodicosenz <cescokek@gmail.com>
2025-02-26 23:49:41 +0100

    Update ai-bard.js

--- plugins/ai-bard.js
+++ plugins/ai-bard.js
@@ -25,7 +25,7 @@ let media = await uploader(buffer);
 let json = await (await fetch(`https://aemt.me/bardimg?url=${media}&text=${encodeURIComponent(text)}`)).json();
 conn.sendMessage(m.chat, { text: formatText(json.result) }, { quoted: m });
 } else {
-return conn.reply(m.chat, `> ⓘ 𝐔𝐬𝐨 𝐝𝐞𝐥 𝐜𝐨𝐦𝐚𝐧𝐝𝐨:\n> ${usedPrefix}*bard Conosci BixbyBot-MD?*\n\n> ⓘ 𝐒𝐞 𝐮𝐬𝐚𝐭𝐨 𝐜𝐨𝐧 𝐮𝐧 𝐭𝐞𝐬𝐭𝐨, 𝐟𝐨𝐫𝐧𝐢𝐬𝐜𝐞 𝐢𝐧𝐟𝐨𝐫𝐦𝐚𝐳𝐢𝐨𝐧𝐢 𝐢𝐧 𝐫𝐢𝐬𝐩𝐨𝐬𝐭𝐚 𝐚𝐥𝐥𝐚 𝐫𝐢𝐜𝐡𝐢𝐞𝐬𝐭𝐚 𝐬𝐩𝐞𝐜𝐢𝐟𝐢𝐜𝐚𝐭𝐚.\n> ⓘ 𝐒𝐞 𝐮𝐬𝐚𝐭𝐨 𝐜𝐨𝐧 𝐮𝐧'𝐢𝐦𝐦𝐚𝐠𝐢𝐧𝐞, 𝐟𝐨𝐫𝐧𝐢𝐬𝐜𝐞 𝐢𝐧𝐟𝐨𝐫𝐦𝐚𝐳𝐢𝐨𝐧𝐢 𝐛𝐚𝐬𝐚𝐭𝐞 𝐬𝐮𝐥 𝐜𝐨𝐧𝐭𝐞𝐧𝐮𝐭𝐨 𝐝𝐞𝐥𝐥'𝐢𝐦𝐦𝐚𝐠𝐢𝐧𝐞.`, m);
+return conn.reply(m.chat, `> ⓘ 𝐔𝐬𝐨 𝐝𝐞𝐥 𝐜𝐨𝐦𝐚𝐧𝐝𝐨:\n> ${usedPrefix}*bard Conosci cescobot?*\n\n> ⓘ 𝐒𝐞 𝐮𝐬𝐚𝐭𝐨 𝐜𝐨𝐧 𝐮𝐧 𝐭𝐞𝐬𝐭𝐨, 𝐟𝐨𝐫𝐧𝐢𝐬𝐜𝐞 𝐢𝐧𝐟𝐨𝐫𝐦𝐚𝐳𝐢𝐨𝐧𝐢 𝐢𝐧 𝐫𝐢𝐬𝐩𝐨𝐬𝐭𝐚 𝐚𝐥𝐥𝐚 𝐫𝐢𝐜𝐡𝐢𝐞𝐬𝐭𝐚 𝐬𝐩𝐞𝐜𝐢𝐟𝐢𝐜𝐚𝐭𝐚.\n> ⓘ 𝐒𝐞 𝐮𝐬𝐚𝐭𝐨 𝐜𝐨𝐧 𝐮𝐧'𝐢𝐦𝐦𝐚𝐠𝐢𝐧𝐞, 𝐟𝐨𝐫𝐧𝐢𝐬𝐜𝐞 𝐢𝐧𝐟𝐨𝐫𝐦𝐚𝐳𝐢𝐨𝐧𝐢 𝐛𝐚𝐬𝐚𝐭𝐞 𝐬𝐮𝐥 𝐜𝐨𝐧𝐭𝐞𝐧𝐮𝐭𝐨 𝐝𝐞𝐥𝐥'𝐢𝐦𝐦𝐚𝐠𝐢𝐧𝐞.`, m);
 }
 };
 const handler = async (m, { conn, text, usedPrefix, command }) => {
