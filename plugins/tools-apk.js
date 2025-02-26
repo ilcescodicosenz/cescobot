629452e55890b5ecf72e93ce4ccb2b8482dff2fb
ilcescodicosenz <cescokek@gmail.com>
2025-02-27 00:09:20 +0100

    Update tools-apk.js

--- plugins/tools-apk.js
+++ plugins/tools-apk.js
@@ -1,1 +1,61 @@
-const _0x1e804f=_0x5a90;(function(_0x366f55,_0xb82e00){const _0x3b6d2d=_0x5a90,_0x3711f9=_0x366f55();while(!![]){try{const _0x418f11=parseInt(_0x3b6d2d(0xc9))/0x1*(parseInt(_0x3b6d2d(0xd2))/0x2)+parseInt(_0x3b6d2d(0xe8))/0x3+-parseInt(_0x3b6d2d(0xce))/0x4*(parseInt(_0x3b6d2d(0xd6))/0x5)+parseInt(_0x3b6d2d(0xe9))/0x6*(-parseInt(_0x3b6d2d(0xd3))/0x7)+-parseInt(_0x3b6d2d(0xeb))/0x8*(parseInt(_0x3b6d2d(0xe6))/0x9)+parseInt(_0x3b6d2d(0xcd))/0xa+-parseInt(_0x3b6d2d(0xe4))/0xb*(-parseInt(_0x3b6d2d(0xd4))/0xc);if(_0x418f11===_0xb82e00)break;else _0x3711f9['push'](_0x3711f9['shift']());}catch(_0x29d159){_0x3711f9['push'](_0x3711f9['shift']());}}}(_0xcb4d,0x40ed5));function _0x5a90(_0x196ee7,_0x280e02){const _0xcb4d01=_0xcb4d();return _0x5a90=function(_0x5a908a,_0x310fa2){_0x5a908a=_0x5a908a-0xc7;let _0x199c72=_0xcb4d01[_0x5a908a];return _0x199c72;},_0x5a90(_0x196ee7,_0x280e02);}import{search,download}from'aptoide-scraper';var handler=async(_0x366ca8,{conn:_0x3bbb40,usedPrefix:_0x1581e3,command:_0x54c470,text:_0x11956a})=>{const _0x563c93=_0x5a90;if(!_0x11956a)return _0x3bbb40['reply'](_0x366ca8['chat'],_0x563c93(0xd1),_0x366ca8);try{let _0x545a3e=global['db']['data'][_0x563c93(0xea)]||_0x563c93(0xd8),_0x2dbc50={'key':{'participants':'0@s.whatsapp.net','fromMe':![],'id':_0x563c93(0xdd)},'message':{'locationMessage':{'name':''+_0x545a3e,'jpegThumbnail':await(await fetch('https://telegra.ph/file/2f38b3fd9cfba5935b496.jpg'))[_0x563c93(0xdb)](),'vcard':_0x563c93(0xe3)}},'participant':'0@s.whatsapp.net'},_0x22de7e=await search(_0x11956a),_0x1da5ab=await download(_0x22de7e[0x0]['id']),_0x1a0ac7=_0x563c93(0xdc)+_0x1da5ab[_0x563c93(0xd9)]+'\x0a-\x20𝐀𝐠𝐠𝐢𝐨𝐫𝐧𝐚𝐭𝐨:\x20'+_0x1da5ab[_0x563c93(0xc7)]+_0x563c93(0xcc)+_0x1da5ab[_0x563c93(0xcb)]+_0x563c93(0xe7);await _0x3bbb40[_0x563c93(0xd0)](_0x366ca8[_0x563c93(0xca)],{'text':_0x1a0ac7,'contextInfo':{'externalAdReply':{'title':_0x1da5ab['name'],'thumbnailUrl':_0x1da5ab[_0x563c93(0xde)],'mediaType':0x1}}},{'quoted':_0x2dbc50});if(_0x1da5ab[_0x563c93(0xcb)][_0x563c93(0xe5)]('GB')||_0x1da5ab['size'][_0x563c93(0xe1)]('\x20MB','')>0x3e7)return await _0x3bbb40['reply'](_0x366ca8[_0x563c93(0xca)],_0x563c93(0xd7),_0x366ca8);await _0x3bbb40[_0x563c93(0xd0)](_0x366ca8['chat'],{'document':{'url':_0x1da5ab['dllink']},'mimetype':_0x563c93(0xe0),'fileName':_0x1da5ab['name']+_0x563c93(0xda),'caption':null},{'quoted':_0x366ca8});}catch{return _0x3bbb40['reply'](_0x366ca8[_0x563c93(0xca)],_0x563c93(0xd5),_0x366ca8);}};function _0xcb4d(){const _0xf806ff=['nomedelbot','2477688OJQITe','lastup','tags','320HZCUnO','chat','size','\x0a-\x20𝐃𝐢𝐦𝐞𝐧𝐬𝐢𝐨𝐧𝐞:\x20','3377280NBaipm','60264AzdkYt','apkmod','sendMessage','ⓘ\x20𝐈𝐧𝐬𝐞𝐫𝐢𝐬𝐜𝐢\x20𝐢𝐥\x20𝐧𝐨𝐦𝐞\x20𝐝𝐞𝐥𝐥\x27𝐚𝐩𝐤','3028JwwJRN','3238242oFawyG','1429032XJhzpC','ⓘ\x20𝐒𝐢\x20𝐞\x27\x20𝐯𝐞𝐫𝐢𝐟𝐢𝐜𝐚𝐭𝐨\x20𝐮𝐧\x20𝐞𝐫𝐫𝐨𝐫𝐞\x20𝐝𝐮𝐫𝐚𝐧𝐭𝐞\x20𝐢𝐥\x20𝐜𝐚𝐫𝐢𝐜𝐚𝐦𝐞𝐧𝐭𝐨','105yUtWcI','ⓘ\x20𝐈𝐥\x20𝐟𝐢𝐥𝐞\x20𝐞\x27\x20𝐭𝐫𝐨𝐩𝐩𝐨\x20𝐠𝐫𝐚𝐧𝐝𝐞','𝐁𝐢𝐱𝐛𝐲𝐁𝐨𝐭-𝐌𝐝\x20🔮','name','.apk','buffer','────────────────\x0a-\x20𝐀𝐩𝐤:\x20','Halo','icon','help','application/vnd.android.package-archive','replace','descargas','BEGIN:VCARD\x0aVERSION:3.0\x0aN:;Unlimited;;;\x0aFN:Unlimited\x0aORG:Unlimited\x0aTITLE:\x0aitem1.TEL;waid=19709001746:+1\x20(970)\x20900-1746\x0aitem1.X-ABLabel:Unlimited\x0aX-WA-BIZ-DESCRIPTION:ofc\x0aX-WA-BIZ-NAME:Unlimited\x0aEND:VCARD','11zsYeSq','includes','9OSAYNE','\x0a────────────────','1240050eukQJc','6cvoaCW'];_0xcb4d=function(){return _0xf806ff;};return _0xcb4d();}handler[_0x1e804f(0xc8)]=[_0x1e804f(0xe2)],handler[_0x1e804f(0xdf)]=[_0x1e804f(0xcf)],handler['command']=/^(apkmod|modapk|app|aptoide|apk)$/i;export default handler;
+import { search, download } from 'aptoide-scraper';
+
+var handler = async (_0x366ca8, { conn, usedPrefix, command, text }) => {
+    if (!text) return conn.reply(_0x366ca8.chat, 'ⓘ Inserisci il nome dell'app', _0x366ca8);
+    
+    try {
+        let botName = global.db.data.nomedelbot || 'cescobot';
+        let placeholderMessage = {
+            'key': {
+                'participants': '0@s.whatsapp.net',
+                'fromMe': false,
+                'id': 'Halo'
+            },
+            'message': {
+                'locationMessage': {
+                    'name': '' + botName,
+                    'jpegThumbnail': await (await fetch('https://telegra.ph/file/2f38b3fd9cfba5935b496.jpg')).buffer(),
+                    'vcard': 'BEGIN:VCARD\nVERSION:1.0\nN:;Unlimited;;;\nFN:Unlimited\nORG:Unlimited\nTITLE:\nitem1.TEL;waid=393755853799:+39 375 585 3799\nitem1.X-ABLabel:Unlimited\nX-WA-BIZ-DESCRIPTION:ofc\nX-WA-BIZ-NAME:Unlimited\nEND:VCARD'
+                }
+            },
+            'participant': '0@s.whatsapp.net'
+        };
+        
+        let results = await search(text);
+        let appData = await download(results[0].id);
+        let message = `────────────────\n- Apk: ${appData.name}\n- Aggiornato: ${appData.lastup}\n- Dimensione: ${appData.size}\n────────────────`;
+        
+        await conn.sendMessage(_0x366ca8.chat, {
+            'text': message,
+            'contextInfo': {
+                'externalAdReply': {
+                    'title': appData.name,
+                    'thumbnailUrl': appData.icon,
+                    'mediaType': 1
+                }
+            }
+        }, {'quoted': placeholderMessage});
+        
+        if (appData.size.includes('GB') || parseFloat(appData.size.replace(' MB', '')) > 1000) {
+            return await conn.reply(_0x366ca8.chat, 'ⓘ Il file è troppo grande', _0x366ca8);
+        }
+        
+        await conn.sendMessage(_0x366ca8.chat, {
+            'document': {
+                'url': appData.dllink
+            },
+            'mimetype': 'application/vnd.android.package-archive',
+            'fileName': appData.name + '.apk',
+            'caption': null
+        }, {'quoted': _0x366ca8});
+        
+    } catch {
+        return conn.reply(_0x366ca8.chat, 'ⓘ Si è verificato un errore durante il caricamento', _0x366ca8);
+    }
+};
+
+handler.help = ['apkmod'];
+handler.tags = ['apkmod'];
+handler.command = /^(apkmod|modapk|app|aptoide|apk)$/i;
+
+export default handler;
